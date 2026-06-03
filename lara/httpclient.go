664e3993690cfb3d67341fa3bad7d4ca6de7e736
davc0n <57829860+davc0n@users.noreply.github.com>
2026-06-03 11:26:01 +0200

    v1.5.0

--- lara/httpclient.go
+++ lara/httpclient.go
@@ -40,7 +40,7 @@ func newClient(auth interface{}, baseURL string) *Client {
 		baseURL:    strings.TrimRight(baseURL, "/"),
 		httpClient: &http.Client{},
 		sdkName:    "lara-go",
-		sdkVersion: "1.4.0",
+		sdkVersion: "1.5.0",
 	}
 
 	// Set authentication method based on type
