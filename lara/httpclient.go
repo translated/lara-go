673aebe5c2ef1cfcc6519fd1fda2bd5e6e673fc1
davc0n <57829860+davc0n@users.noreply.github.com>
2026-07-20 11:45:40 +0200

    v1.6.0

--- lara/httpclient.go
+++ lara/httpclient.go
@@ -41,7 +41,7 @@ func newClient(auth interface{}, baseURL, sessionID string) *Client {
 		baseURL:    strings.TrimRight(baseURL, "/"),
 		httpClient: &http.Client{},
 		sdkName:    "lara-go",
-		sdkVersion: "1.5.2",
+		sdkVersion: "1.6.0",
 		sessionID:  sessionID,
 	}
 
